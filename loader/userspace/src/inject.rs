4df5de5fb8a55503a7b136c8f6be58095cb155d3
mufanc <mufanc.xyz@gmail.com>
2024-03-20 00:13:06 +0800

    refactor: args retrieving

--- loader/userspace/src/inject.rs
+++ loader/userspace/src/inject.rs
@@ -1,47 +1,57 @@
+#[cfg(target_arch = "aarch64")]
 use std::mem;
+
 use std::mem::MaybeUninit;
 use anyhow::Result;
 use log::{debug, warn};
 use nix::errno::Errno;
 use nix::libc;
-use nix::libc::{iovec, user_regs_struct};
+
+#[cfg(target_arch = "aarch64")]
+use nix::libc::iovec;
+
+use nix::libc::user_regs_struct;
 use nix::sys::ptrace;
 use nix::unistd::Pid;
 
-struct Registers {
-    pid: Pid,
-    regs: user_regs_struct,
-}
+#[derive(Clone)]
+struct Registers(user_regs_struct);
 
 impl Registers {
-    fn new(pid: Pid, regs: user_regs_struct) -> Self {
-        Self { pid, regs }
+    fn new(regs: user_regs_struct) -> Self {
+        Self(regs)
     }
 
     #[cfg(target_arch = "x86_64")]
-    fn arg(&self, n: usize) -> Result<u64> {
-        Ok(match n {
-            0 => self.regs.rdi,
-            1 => self.regs.rsi,
-            2 => self.regs.rdx,
-            3 => self.regs.rcx,
-            4 => self.regs.r8,
-            5 => self.regs.r9,
-            _ => {
-                let n = (n - 6) as u64;
-                ptrace::read(self.pid, (self.regs.rsp + 8 * n + 16) as _)? as u64
-            },
-        })
+    fn arg(&self, n: usize) -> u64 {
+        match n {
+            0 => self.0.rdi,
+            1 => self.0.rsi,
+            2 => self.0.rdx,
+            3 => self.0.rcx,
+            4 => self.0.r8,
+            5 => self.0.r9,
+            _ => unreachable!(),
+        }
     }
 
     #[cfg(target_arch = "aarch64")]
-    fn arg(&self, n: usize) -> Result<u64> {
-        Ok(if n < 8 {
-            self.regs.regs[n]
+    fn arg(&self, n: usize) -> u64 {
+        if n < 8 {
+            self.0.regs[n]
         } else {
-            let n = (n - 8) as u64;
-            ptrace::read(self.pid, (self.regs.sp + 8 * n) as _)? as u64
-        })
+            unreachable!()
+        }
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    fn sp(&self) -> u64 {
+        self.0.rsp
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    fn sp(&self) -> u64 {
+        self.0.sp
     }
 }
 
@@ -63,10 +73,10 @@ impl Tracee {
         let mut regs: MaybeUninit<user_regs_struct> = MaybeUninit::uninit();
 
         Errno::result(unsafe {
-            libc::ptrace(libc::PTRACE_GETREGS, self.0.as_raw(), 1 /* NT_PRSTATUS */, regs.as_mut_ptr())
+            libc::ptrace(libc::PTRACE_GETREGS, self.0.as_raw(), 0, regs.as_mut_ptr())
         })?;
 
-        Ok(Registers::new(self.0, unsafe { regs.assume_init() }))
+        Ok(Registers::new(unsafe { regs.assume_init() }))
     }
 
     #[cfg(target_arch = "aarch64")]
@@ -81,7 +91,33 @@ impl Tracee {
             libc::ptrace(libc::PTRACE_GETREGSET, self.0.as_raw(), 1 /* NT_PRSTATUS */, &iov as *const _)
         })?;
 
-        Ok(Registers::new(self.0, unsafe { regs.assume_init() }))
+        Ok(Registers::new(unsafe { regs.assume_init() }))
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    fn uprobe_arg(&self, n: usize) -> Result<u64> {
+        let regs = self.regs()?;
+        let arg = if n < 6 {
+            regs.arg(n)
+        } else {
+            let n = (n - 6) as u64;
+            ptrace::read(self.0, (regs.sp() + 8 * n + 16 /* call + push rbp */) as _)? as u64
+        };
+        
+        Ok(arg)
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    fn uprobe_arg(&self, n: usize) -> Result<u64> {
+        let regs = self.regs()?;
+        let arg = if n < 8 {
+            regs.arg(n)
+        } else {
+            let n = (n - 8) as u64;
+            ptrace::read(self.0, (regs.sp() + 8 * n) as _)? as u64
+        };
+        
+        Ok(arg)
     }
 }
 
@@ -96,11 +132,9 @@ impl Drop for Tracee {
 pub fn check_process(pid: i32) -> Result<()> {
     let tracee = Tracee::new(pid);
     tracee.attach()?;
-
-    let regs = tracee.regs()?;
     
     for i in 0 ..= 12 {
-        let arg = regs.arg(i)?;
+        let arg = tracee.uprobe_arg(i)?;
         debug!("check_process: arg[{i:2}] = 0x{arg:x}\t({arg})");
     }
 
