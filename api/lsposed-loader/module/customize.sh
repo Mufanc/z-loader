f740a106ae1a1591645cdf11d11902fb202410c8
Mufanc <mufanc.xyz@gmail.com>
2024-04-13 15:11:19 +0800

    feat: verify kernel version in customize.sh

--- api/lsposed-loader/module/customize.sh
+++ api/lsposed-loader/module/customize.sh
@@ -4,3 +4,12 @@ if [ "$API" -lt 31 ]; then
 else
     ui_print "- Device SDK: $API"
 fi
+
+KVER=$(getprop ro.kernel.version)
+
+if [ "${KVER//./}" -lt 510 ]; then
+    ui_print "! Unsupported kernel version: $KVER"
+    abort    "! Kernel version must be 5.10 or higher"
+else
+    ui_print "- Kernel version: $KVER"
+fi
