61617ef0a2cb2db76aa1d0fe7ec3ff4fa0a8748d
Mufanc <mufanc.xyz@gmail.com>
2024-04-14 15:04:40 +0800

    feat: update customize.sh

--- api/lsposed-loader/module/customize.sh
+++ api/lsposed-loader/module/customize.sh
@@ -5,11 +5,12 @@ else
     ui_print "- Device SDK: $API"
 fi
 
-KVER=$(getprop ro.kernel.version | tr '.' ' ' | xargs printf '%02d')
+KVER=$(uname -r | awk -F- '{ print $1 }')
+KVER_INT=$(getprop ro.kernel.version | tr '.' ' ' | xargs printf '%02d')
 
-if [ "$KVER" -lt 510 ]; then
-    ui_print "! Unsupported kernel version: $(uname -r)"
+if [ "$KVER_INT" -lt 510 ]; then
+    ui_print "! Unsupported kernel version: $KVER"
     abort    "! Kernel version must be 5.10 or higher"
 else
-    ui_print "- Kernel version: $(uname -r)"
+    ui_print "- Kernel version: $KVER"
 fi
